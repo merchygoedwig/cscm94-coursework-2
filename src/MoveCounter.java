4698cff03f7468e50ade4f2c3e433d32386ab835
Genevieve <genevieve@becquerel.me>
2021-04-03 18:34:59 +0100

    added documentation required

--- src/MoveCounter.java
+++ src/MoveCounter.java
@@ -1,5 +1,19 @@
+/**
+ * MoveCounter extends the HoleAction abstract class and implements
+ * rule actions for interactions involving a Player moving Counters
+ * into a Hole.
+ *
+ * @author Genevieve Clifford
+ * @version 1.0
+ */
 public class MoveCounter extends HoleAction {
-
+    /**
+     * Default constructor for MoveCounter, this is to be used in the context
+     * of a Hand belonging to a Player (each Hand will have an instance of
+     * MoveCounter for checking valid moves).
+     * @param player the instance of Player that originated the action
+     * @param hole the hole which a Player has targeted
+     */
     public MoveCounter(Player player, Hole hole) {
         originator = player;
         target = hole;
@@ -17,6 +31,12 @@ public class MoveCounter extends HoleAction {
         return !(target instanceof Mancala);
     }
 
+    /**
+     * Checks if a move in the instance of MoveCounter is a valid move that
+     * can be made at that time.
+     * @return Boolean of if counters are allowed to be moved from a Hole by
+     * a given Player.
+     */
     @Override
     public Boolean checkValidity() {
         return (
--- src/Rule.java
+++ src/Rule.java
@@ -1,3 +1,15 @@
-interface Rule {
+/**
+ * Rules are used to check the validity of moves during a game, each action
+ * is associated with a Rule (or sub-set of it).
+ *
+ * @author Genevieve Clifford
+ * @version 1.0
+ */
+public interface Rule {
+    /**
+     * Checks for if a rule is violated by a move or not
+     * @return Boolean representation of the validity of a given rule given
+     * the instance's originator and target values.
+     */
     Boolean checkValidity();
 }
