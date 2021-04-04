4d70862d206fd6009751b0de2df648a3b77e2e83
IsaacSH <isaacwood1106@gmail.com>
2021-04-04 18:15:35 +0100

    add adminstrator class

---
+++ src/Administrator.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+
+/**
+ * Administrator is a user and grant right to player for playing
+ * @author  Siu Ho,Wu
+ * @version 1.0
+ */
+
+
+public class Administrator {
+	
+
+	private ArrayList<Player> players = new ArrayList<>();
+	
+	/**
+	 * 
+	 * add player to the requested list when called by player.
+	 */
+	public void requestToPlay(Player p) {
+		
+		players.add(p);
+	}
+    
+	/**
+	 * getter class for showing player request at administrator's page.
+	 * @return players who made requests
+	 */
+	public ArrayList<Player> getPlayer(){
+		
+		return players;
+	}
+
+	/**
+	 * 
+	 * assign a player's player statue be true;
+	 */
+   void assigned(Player p) {
+    	    	   	
+       // p.setAssigned(true);
+    }
+}
