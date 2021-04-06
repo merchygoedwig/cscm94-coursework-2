a84b17318247d9537458b26ec744f313ddba9556
Genevieve <genevieve@becquerel.me>
2021-04-06 13:11:12 +0100

    (for Josh) added session class

--- src/com/group8/mancala/java/Session.java
+++ src/com/group8/mancala/java/Session.java
@@ -1,4 +1,26 @@
-package com.group8.mancala.java;
+/**
+ * Session tracks the results of each player, it collaborates with game.
+ *
+ * @author Joshua Thurman
+ * @version 1.0
+ */
 
-public interface Session {
+import java.io.File;
+
+public interface Session1 {
+    public int winPercentage(int i, int n);
+
+    public Game startGame();
+
+    public Game1 saveGame();
+
+    public Game2 endGame();
+
+    public Players getMostFrequentPlayers();
+
+    public PowerUps getLastPowerUps();
+
+    public Changes persistChanges();
+
+    public File readFile();
 }
