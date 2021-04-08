42a35444abf6f94554c4bc9063c5f6949c770961
Genevieve Clifford <genevieve@becquerel.me>
2021-04-08 20:43:05 +0100

    oops, forgot to remove some git merge silliness

--- src/main/java/com/group8/mancala/playerfacing/GameBoard.java
+++ src/main/java/com/group8/mancala/playerfacing/GameBoard.java
@@ -1,11 +1,6 @@
-<<<<<<< HEAD:src/main/java/com/group8/mancala/playerfacing/GameBoard.java
 package com.group8.mancala.playerfacing;
 
 import com.group8.mancala.gameplayobjects.Hole;
-=======
-package com.group8.mancala;
->>>>>>> move-refactor-to-maven:src/main/java/com/group8/mancala/GameBoard.java
-
 import java.util.LinkedList;
 
 abstract class GameBoard {
