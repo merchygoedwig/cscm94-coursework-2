1b617f15abf38f6723711950f230e9af8956aca6
Paulina Mielewska <mielewskapaulina@gmail.com>
2021-04-13 18:52:04 +0200

    Abstract methods do not specify a body

--- src/main/java/com/group8/mancala/gameplayobjects/Hole.java
+++ src/main/java/com/group8/mancala/gameplayobjects/Hole.java
@@ -1,9 +1,54 @@
+/**
+FILENAME: Hole.java 
+Defines a class Hole which extends class Mancala to store information about a single hole and counters it contains.
+It contains an additional method 
+@author Paulina Mielewska
+*/
 package com.group8.mancala.gameplayobjects;
 
+import java.util.*;
 import com.group8.mancala.playerfacing.Player;
 
 public interface Hole {
-    Player getAssignedPlayer();
+  public Player assignedPlayer;
+  public Stack<Counter> counters;
+  int counterAmount;
 
-    int getCounterCount();
+
+  /**
+   * Accepts new counters that are added to a Hole during the game.
+   *
+   * @param someCounter Counter thats has been moved from other Hole
+   */   
+  private void acceptCounter(Counter someCounter) {
+    Counter counter= new Counter(someCounter);
+    counters.push(counter);
+    counterAmount++;
+  }
+  
+  /**
+   * Remove all counters from a hole.
+   */
+  private void giveUpCounter() {
+    counterAmount = 0;
+  }
+
+  /**
+   * Returns the number of counters in a given hole.
+   *
+   * @return An int representing the number of counters in a hole.
+   */
+  public int getCounterCount() {
+    return counterAmount;
+  }
+  /**
+   * Method to return the current player that can play and move the counters from the current hole.
+   *
+   * @return Player that has been assigned to the part of the board with the Hole.
+   */  
+  public Player getAssignedPlayer() {
+    return assigned Player;   
+  }
 }
+
+
--- src/main/java/com/group8/mancala/gameplayobjects/Mancala.java
+++ src/main/java/com/group8/mancala/gameplayobjects/Mancala.java
@@ -1,4 +1,17 @@
+/**
+FILENAME: /mancala.java 
+Defines a class Mancala which stores information about a final mancala hole and counters it contains.
+@author Paulina Mielewska
+*/
 package com.group8.mancala.gameplayobjects;
 
-public interface Mancala {
-}
+
+public interface Mancala extends Hole {
+  boolean isMancala = true;
+  
+  private boolean isMancala() {
+    return isMancala;
+  }
+  }
+
+
